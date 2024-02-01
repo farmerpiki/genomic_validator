e725aa1133377bf6e069d54114e46dd9bb48cd22
Radu Benea <radu@binarzone.com>
2024-02-01 15:16:32 +0200

    allow alternative sequences

--- genomic_validator.cxx
+++ genomic_validator.cxx
@@ -263,22 +263,14 @@ bool isBoolean(std::string_view value)
 
 bool isHumanChromosome(std::string_view chrom)
 {
-	// clang-format off
-    static const std::set<std::string_view> humanChromosomesWithoutPrefix = {
-        "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", 
-        "11", "12", "13", "14", "15", "16", "17", "18", "19", 
-        "20", "21", "22", "X", "Y", "MT"
-    };
+	// Pattern covers standard chromosomes, "chr" prefix, mitochondrial DNA,
+	// and alternative sequences like "chr1_KI270706v1_random"
+	static std::regex const chromPattern(
+		"^(chr)?(\\d{1,2}|X|Y|MT|"
+		"M)([_\\-][A-Za-z0-9]+)*$",
+		std::regex_constants::ECMAScript | std::regex_constants::icase);
 
-    static const std::set<std::string_view> humanChromosomesWithPrefix = {
-        "chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10", 
-        "chr11", "chr12", "chr13", "chr14", "chr15", "chr16", "chr17", "chr18", "chr19", 
-        "chr20", "chr21", "chr22", "chrX", "chrY", "chrM"
-    };
-	// clang-format on
-
-	return humanChromosomesWithoutPrefix.find(chrom) != humanChromosomesWithoutPrefix.end()
-		|| humanChromosomesWithPrefix.find(chrom) != humanChromosomesWithPrefix.end();
+	return std::regex_search(chrom.cbegin(), chrom.cend(), chromPattern);
 }
 
 bool checkFormatAndSamples(std::vector<std::string_view> const &fields, size_t formatIndex)
